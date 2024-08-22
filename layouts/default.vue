f5aca5596ad30fd0e30777a5bb3182abe9bb379d
Czedrix Barcena <czedrixb@gmail.com>
2024-08-22 13:54:44 +0800

    to be deployed in vercel

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="bg-spotifyDark h-full flex items-center">
-    <div class="container mx-auto py-24 md:py-28 xl:py-40">
-      <slot />
-    </div>
+  <div class="bg-spotifyDark">
+    <slot />
   </div>
 </template>
